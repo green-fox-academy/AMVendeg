5115d9097bd9299ef9bcf67d391f186b2b7f43a6
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2021-10-20 21:30:30 +0200

    TS exercises vol.2
    Introduce, two-numbers, coding-hours

---
+++ ts-gyakorlo/exercise.ts
@@ -0,0 +1,33 @@
+'use strict';
+
+// INTRODUCE exercise
+
+console.log('Davy Jones ' + ' = AM Vendeg');        // összefűztem
+console.log(37 -1);                                 // mert ez így jobban néz ki :)  test = true :) 
+console.log(1.71 + " cm");                          // mértékegységgel, különben a volt matektanárom visszakérdezne, h 1.71 mi?! krumpli, gömb, pizzaszelet?
+
+
+// TWO-NUMBERS exercise - milyen szerencse h a 13 a szerencse számom :) 
+
+console.log(13 + 22);
+console.log(22 - 13);
+console.log(22 * 13);
+console.log(22 / 13);
+console.log(22 % 13); 
+
+
+// CODING-HOURS exercise
+// An average Green Fox attendee codes 6 hours daily ---- dejó neki! 
+// The semester is 17 weeks long
+// Exercise: how many hours is spent with coding in a semester
+// How many hours if the attendee only codes on workdays
+// Percentage of the coding hours in the semester if the average work hours weekly is 52
+
+console.log('17 weeks coding 6 hours per day is: ' + 17*(6*7)) + ' h.';                                      // zárójeles szorzást is tud, nahát! 
+console.log(17*7*6);                                                                                         // ez a zárójeles szorzás ellenőrzése 
+console.log('If she codes only workdays, the number of the hours is: ' + 17*(6*5) + 'h.');                   // ha csak workdays, akkor 6 * 5, nem 6*7
+
+console.log('Coding hours in percentage, if 52 the average per week: ' + (816 / 884 *100) + '%.');           // napi 6 órával 1 héten: 6*7=48 óra, 52 azt jelenti h +4 óra hetente
+console.log(816/884*100);                                                                                    // 17*48=816, 17*52=884 - de ebben a % eredményben nem vagyok biztos :) 
+
+
