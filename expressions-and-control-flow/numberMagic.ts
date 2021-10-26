23af95ccaf78f5a2d8057497486c2a0650a5d475
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2021-10-26 12:56:43 +0200

    extra saját példa operátorokhoz: numberMagic
    nyumberMagic.ts megmondja h mennyi az eredmény ha az adott számot osztod 13-al, vagy visszaszól, ha nem számot adtál meg :)

---
+++ expressions-and-control-flow/numberMagic.ts
@@ -0,0 +1,16 @@
+'use strict';
+
+// gimme your number and i will tell, is it divide by 13. 
+
+let yourNumber: any = "ezmiez";
+
+if (yourNumber /= 13) {
+
+    console.log("Congratulations! Your number / 13 is: " + yourNumber);
+
+} else {
+    
+    console.log(typeof(yourNumber) + " It's not a number, cheaty! :)");
+}
+
+
