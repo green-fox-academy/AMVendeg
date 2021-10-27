1bddd2d322f7d5625c75219e28d018bba352e40b
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2021-10-27 10:19:24 +0200

    updated conditions for numberMagic
    módosítottam a feltételeken

--- expressions-and-control-flow/numberMagic.ts
+++ expressions-and-control-flow/numberMagic.ts
@@ -1,16 +1,24 @@
 'use strict';
 
+
 // gimme your number and i will tell, is it divide by 13. 
 
-let yourNumber: any = "ezmiez";
+let yourNumber: any = 0;
 
-if (yourNumber /= 13) {
 
-    console.log("Congratulations! Your number / 13 is: " + yourNumber);
+if (isNaN(yourNumber) || yourNumber === true || yourNumber === false) {
 
-} else {
+    console.log(" It's not a num, Bear! :) ");
+  
+}   else if (yourNumber == 0) {
+
+        console.log("Okay, but 13 / 0 always will be 0.")
+}
     
-    console.log(typeof(yourNumber) + " It's not a number, cheaty! :)");
+ else {
+        
+    console.log("Congratulations! Your number: " + yourNumber + " / 13 is: " + yourNumber / 13);
+
 }
 
 
