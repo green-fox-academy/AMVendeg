80f2a410d73b6432f0e503f47a4abccf5b181831
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2021-11-03 09:32:56 +0100

    Increment one element in an array
    Egy elem növelése a tömbben

---
+++ arrays/increment-element.ts
@@ -0,0 +1,11 @@
+// -  Create a variable named `numbers` with the following content: `[1, 2, 3, 4, 5]`
+// -  Increment the third element directly (without loops and without array methods: forEach, map, ...)
+// -  Log the third element to the console
+
+// i am not pretty sure I uderstand the description clearly, but I increment the third element (counted form 0 index.)
+let numbers: number[] = [1, 2, 3, 4, 5];
+
+numbers[3] = numbers[3] + 1;
+
+console.log(numbers[3]);
+console.log(numbers);
