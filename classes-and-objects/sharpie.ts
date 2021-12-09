ec2001acf5fe469090ff078efbb9f393d8ba876d
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2021-12-09 18:52:21 +0100

    Sharpie according to prettier code format
    (2 spaces instead of 4)

--- classes-and-objects/sharpie.ts
+++ classes-and-objects/sharpie.ts
@@ -8,21 +8,20 @@ Every sharpie is created with a default inkAmount value: 100
 We can use() the sharpie objects: using it decreases inkAmount by 10
 */
 
-
 export class Sharpie {
-    color: string;
-    width: number;
-    inkAmount: number
+  color: string;
+  width: number;
+  inkAmount: number
 
-    constructor (color: string = '', width: number = 0, inkAmount: number = 10) {
-        this.color = color;
-        this.width = width;
-        this.inkAmount = inkAmount;
-    }
+  constructor (color: string = '', width: number = 0, inkAmount: number = 10) {
+    this.color = color;
+    this.width = width;
+    this.inkAmount = inkAmount;
+  }
 
-    use() {
-        this.inkAmount -= 10;
-    }
+  use() {
+    this.inkAmount -= 10;
+  }
 }
 
 let mySharpie = new Sharpie('steelblue', 35);
