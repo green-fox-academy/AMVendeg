e12582bccbe180d4106b0408d18a3d37b5ec41dd
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2021-12-18 12:10:21 +0100

    Flower pt 2: needWater(), getWater()

--- Inheritance/Garden/flower.ts
+++ Inheritance/Garden/flower.ts
@@ -5,12 +5,23 @@ class Flower extends Plants {
     constructor(color: string = '') {
         super(color);
     }
+
+    needWater() {
+        if (this.waterAmunt < 5) {
+            console.log(`The flower neeeds more water`);
+        } else {
+            console.log(`The flower doesn't need any water now.`);
+        }
+    }
+
+    getWater(waterAmount:number): void {
+        let waterAbsorb = waterAmount * 0.75;
+        if (this.waterAmunt < 5) {
+            this.waterAmunt += waterAbsorb;
+        }
+    }
+
 }
 export { Flower };
 
-let blueFlower = new Flower;
-let yellowFlower = new Flower;
-blueFlower.color = 'blue';
-yellowFlower.color = 'yellow';
 
-console.log(blueFlower);
