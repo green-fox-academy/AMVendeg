84aa157470a661be8414c1fc910d4cfe1644001b
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2021-12-18 12:14:51 +0100

    Tree pt. 3: addWater(), getWater() ${color} added

--- Inheritance/Garden/tree.ts
+++ Inheritance/Garden/tree.ts
@@ -2,13 +2,25 @@ import { Plants } from './plants';
 
 class Tree extends Plants {
 
-    constructor(color: string = '') {
+    constructor (color: string = '') {
         super(color);
     }
+
+    needWater() {
+        if (this.waterAmunt < 10) {
+            console.log(`The ${this.color} Tree need water`);
+        } else {
+            console.log(`The ${this.color} Tree doesn't need water`);
+        }
+    }
+
+    getWater(waterAmount:number): void {
+        let waterAbsorb = waterAmount * 0.4;
+        if (this.waterAmunt < 10) {
+            this.waterAmunt += waterAbsorb;
+        }
+    }
+
 }
 export { Tree };
 
-let purpleTree = new Tree;
-let orangeTree = new Tree;
-purpleTree.color = 'purple';
-orangeTree.color = 'orange';
