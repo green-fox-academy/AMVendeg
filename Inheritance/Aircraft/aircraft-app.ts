e63450007603c7e735c7f222529b127956cf1206
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-04 17:09:01 +0100

    update: F35 and F16 also created

--- Inheritance/Aircraft/aircraft-app.ts
+++ Inheritance/Aircraft/aircraft-app.ts
@@ -1,7 +1,14 @@
 import { F16 } from './f16-aircraft';
 import { F35 } from './f35-aircraft';
-import { Aircraft } from './aircraft';
+// import { Aircraft } from './aircraft';
 
 let myAircraft = new F35('F35'); 
 myAircraft.refillAmmo(300);
-myAircraft.getAmmunition;
+myAircraft.getAmmunition();
+
+myAircraft.getStatus();
+
+let my16aircraft = new F16('F16');
+my16aircraft.refillAmmo(22);
+
+
