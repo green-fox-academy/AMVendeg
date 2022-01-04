b69d826625eff1ae082faccd3c46722d42af3fbb
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-04 11:11:38 +0100

    methods: figth(), ammoNeed(), refillAmmo(), getAmm

--- Inheritance/Aircraft/aircraft.ts
+++ Inheritance/Aircraft/aircraft.ts
@@ -1,22 +1,52 @@
 // create a class that represents an aircraft
 // There are 2 types of aircrafts: F16 and F35
 // Both aircrafts should keep track of their ammunition
+// All aircrafts should be created with an empty ammo storage
+
 
 class Aircraft {
    public typeOfAircraft: string;
    public ammunition: number;
    public maxAmmo: number;
-   public starterAmmo: number;
+   public zeroAmmo: number;
    public baseDamage: number;
 
    constructor() {
        this.ammunition = 0;
        this.typeOfAircraft = '';
        this.maxAmmo = 0;
-       this.starterAmmo = 0;
+       this.zeroAmmo = 0;
        this.baseDamage = 0;
    }
-  
+
+   // It should use all the ammo (set it to 0) and it should return the damage it causes
+   // damage is calculated by multiplying the base damage by the ammunition
+
+   public fight(): number {
+        this.ammunition = this.zeroAmmo;
+        return this.baseDamage * this.ammunition;
+   }
+
+    // It should take a number as parameter that represents the ammunition coming from a station
+    // Increase the ammo of the aircraft by the right amount
+    // It can't have more ammo than the parameter value or the max ammo of the aircraft
+    // It should return the remaining (unused) ammo
+    // Eg. Filling an empty F35 with 300 would completely fill the storage of the aircraft and would return the remaining 288 
+    
+    ammoNeed():number {
+        return this.maxAmmo - this.ammunition;
+    }
+
+   refillAmmo(incomingAmmo: number): void {
+    this.ammunition += this.ammoNeed();
+    let remainingAmmo = incomingAmmo - this.ammunition;
+    console.log(`Remaining ammo is: ${remainingAmmo}`);
+
+   }
+
+   getAmmunition(): number {
+       return this.ammunition;
+   }
 }
 
 export { Aircraft };
