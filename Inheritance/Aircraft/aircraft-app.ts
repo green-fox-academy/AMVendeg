94453ee7596dfbb3cd37e71efd355d37138a4c08
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-04 11:12:23 +0100

    let my aircraft (példányosítás)

---
+++ Inheritance/Aircraft/aircraft-app.ts
@@ -0,0 +1,7 @@
+import { F16 } from './f16-aircraft';
+import { F35 } from './f35-aircraft';
+import { Aircraft } from './aircraft';
+
+let myAircraft = new F35('F35'); 
+myAircraft.refillAmmo(300);
+myAircraft.getAmmunition;
