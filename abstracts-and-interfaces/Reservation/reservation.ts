79927aa012e5e12dcd901f87da31ef8c1f024bb4
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-07 00:27:00 +0100

    Create reservation interface

---
+++ abstracts-and-interfaces/Reservation/reservation.ts
@@ -0,0 +1,9 @@
+// Create a Reservation class by implementing all the features as required by the interface.
+// Booking# 1WBA3OMU for THU
+
+interface Reservation {
+  getDowBooking(): string;
+  getCodeBooking(): string;
+}
+
+export { Reservation };
