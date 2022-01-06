ec50099284ed409b26457e0fe1611774893da0da
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-06 19:55:52 +0100

    create Domino class impl. Printable

---
+++ abstracts-and-interfaces/Printable/domino.ts
@@ -0,0 +1,24 @@
+// Create a new class called Domino that has two integer fields that represents the two sides of a domino
+// Implement this interface on the Domino and Todo classes
+// The domino should have the following string representation: Domino A side: 3, B side: 2
+
+import { PrintableInterface } from "./printable";
+
+
+class Domino implements PrintableInterface {
+  upperSide: number;
+  downSide: number;
+
+  constructor (upperSide: number, downSide: number) {
+    this.upperSide = upperSide;
+    this.downSide = downSide;
+  }
+
+  printAllFields(): void {
+      
+  }
+
+
+}
+
+export { Domino };
