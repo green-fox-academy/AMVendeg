a313370c284e54f12515fb089dd6c80e5413eaee
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-06 19:33:57 +0100

    create Flyable interface

---
+++ abstracts-and-interfaces/flyable.ts
@@ -0,0 +1,10 @@
+// Create a Flyable interface
+// it should have land, fly and takeOff methods (TakeOff in C#)
+
+interface FlyableInterface {
+    land(): void;
+    fly(): void;
+    // takerOff(): void;
+}
+
+export { FlyableInterface };
