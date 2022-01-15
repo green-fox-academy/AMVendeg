2727189559ef16df904db64982048c22ce8e6ec8
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-15 11:39:34 +0100

    updated for testing findNewAnimal method

--- foundation-example-exam/animal-protection/app.ts
+++ foundation-example-exam/animal-protection/app.ts
@@ -10,5 +10,24 @@ myShelter.rescue(new Dog('Torta'));
 myShelter.animals[1].feedWithSpoiledFood();
 
 console.log(myShelter.heal());
+myShelter.addAdopter('Masni');
+myShelter.addAdopter('Jones');
+myShelter.addAdopter('Dr Schaffhausen');
+
+
+// console.log(myShelter.animals);
+console.log(myShelter.toString());
+myShelter.findNewOwner();
+console.log(myShelter.toString());
+myShelter.findNewOwner();
+console.log(myShelter.toString());
+myShelter.findNewOwner();
+console.log(myShelter.toString());
+myShelter.findNewOwner();
+console.log(myShelter.toString());
+
+
+
+
 
 
