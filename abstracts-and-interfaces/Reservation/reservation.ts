141a76aea3f3376bca5fcc687007df8fe512208a
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-16 19:33:48 +0100

    import Dow class

--- abstracts-and-interfaces/Reservation/reservation.ts
+++ abstracts-and-interfaces/Reservation/reservation.ts
@@ -1,9 +1,9 @@
 // Create a Reservation class by implementing all the features as required by the interface.
 // Booking# 1WBA3OMU for THU
+import { DowCode } from "./dow";
 
-interface Reservation {
+interface Reservation extends DowCode {
   getDowBooking(): string;
   getCodeBooking(): string;
+  printStatus(): string;
 }
-
-export { Reservation };
