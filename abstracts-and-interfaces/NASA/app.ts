fc96c610b0edec7de05fe4c6ce0997d72655bf33
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-21 00:13:45 +0100

    Create my first astronaut :) own exercise

---
+++ abstracts-and-interfaces/NASA/app.ts
@@ -0,0 +1,6 @@
+import { Astronaut } from "./astronaut";
+
+let myAstronaut = new Astronaut('Armin Schaffhausen', 'half-american-german');
+myAstronaut.addRandomSpecialization();
+
+console.log(myAstronaut);
