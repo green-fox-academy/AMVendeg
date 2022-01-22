3a13dd1c994f5be47ea916bc03666487edb6b2b4
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-01-22 14:47:37 +0100

    Updated accordint to Balazs comment

--- arrays/change-elem.ts
+++ arrays/change-elem.ts
@@ -2,12 +2,16 @@
 // -  Change the value of the fourth element (8) to 4 with the `.map` method 
 // -  Print the fourth element as a test
 
-
-// első megoldás: nem volt kikötve hogy a többi szám nem változhat, illetve hogyan lesz belőle 4, ezért igy lett:
+// first soluiton
 let numbers = [1, 2, 3, 8, 5, 6].map(function(e) {
+  if (e === 8) {
     return e / 2;
+  } 
+  return e;
 });
-
-// print the 4th element as test
 console.log(numbers[3]);
+
+
+
+
 
