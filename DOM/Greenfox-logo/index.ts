c89017d8bbb6c553103897df535aee5bbba4fd4c
jones( ) <33423111+AMVendeg@users.noreply.github.com>
2022-02-04 20:57:57 +0100

    Make the fox's eyes red by style.fill tomato

---
+++ DOM/Greenfox-logo/index.ts
@@ -0,0 +1,9 @@
+// Select left and right eyes
+// They have #left-eye and #right-eye
+// turn their color (fill) to tomato
+
+const leftEye = document.getElementById('left-eye');
+const rightEye = document.getElementById('right-eye');
+
+leftEye.style.fill = 'tomato';
+rightEye.style.fill = 'tomato';
