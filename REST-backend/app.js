6cdd6fba71cb86171441a833276fd0ca8205e6a8
jones <mobilizacsio@gmail.com>
2022-02-17 22:31:42 +0100

    doubling is ready

--- REST-backend/app.js
+++ REST-backend/app.js
@@ -1,18 +1,39 @@
 const express = require('express');
 const app = express();
-const PORT = 5500;
+const PORT = 3000;
 const path = require('path');
 
 // middleware helye
 app.use(express.static('assets'));
+// middleware to convert everthing to json
+app.use(express.json());
 
-
+// send the index.html to the client
 app.get('/index.html', (req, res) => {
-  // res.send('OK!');
   res.sendFile(path.join(__dirname, 'index.html'));
-
 });
 
+
+// adding doubling endpoint 
+app.get('/doubling', (req, res) => {
+  let received = parseInt(req.query.input);
+  let result = {};
+
+  if (received) {
+    result = {
+      "received": received,
+      "result": received * 2
+    };
+  } else {
+    result = {
+      error: 'Please provide an input!',
+    };
+  }
+
+  res.json(result);
+});
+
+
 app.listen(PORT, () => {
- console.log(`yaaay, the port is listening this ${PORT}`);
+  console.log(`yaaay, the port is listening this ${PORT}`);
 });
