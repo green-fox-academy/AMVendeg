e6e6da1703f81a74a49cd097a575cebd81c78965
jones <mobilizacsio@gmail.com>
2022-02-22 23:00:21 +0100

    getBooks, renderBooks into index.html

---
+++ ORIENTATION/BackendAPI/bookstore/public/app.ts
@@ -0,0 +1,36 @@
+
+import { Book } from '../books';
+
+const table = document.querySelector('table') as HTMLTableElement;
+
+const BASE_URL = 'http://localhost:3000';
+
+function getBooks(): Promise<Book[]> {
+  return fetch(`${BASE_URL}/booklist`)
+    .then(resp => resp.json())
+    .then(books => books);
+}
+
+// test getBooks: console.log(getBooks());
+
+
+function renderBooks(): void {
+  getBooks().then(books => {
+
+    books.forEach(book => {
+      const tr = document.createElement('tr');
+      tr.appendChild(createFields(`${book.book_id}`));
+      tr.appendChild(createFields(`${book.book_name}`));
+      table.appendChild(tr);
+    });
+  });
+}
+
+
+function createFields(value: string): HTMLTableCellElement {
+  const td = document.createElement('td');
+  td.innerHTML = value;
+  return td;
+}
+
+renderBooks();
