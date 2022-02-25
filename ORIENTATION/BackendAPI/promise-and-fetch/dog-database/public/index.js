1206377f598d7fbcc79a6a6dcaf78f55836b43fd
jones <mobilizacsio@gmail.com>
2022-02-25 22:42:32 +0100

    create ul, and fetch: then, catch

--- ORIENTATION/BackendAPI/promise-and-fetch/dog-database/public/index.js
+++ ORIENTATION/BackendAPI/promise-and-fetch/dog-database/public/index.js
@@ -1,1 +1,15 @@
- console.log('index.js loaded');
+//  console.log('index.js loaded');
+const URL = "http://localhost:3000/dogs";
+const list = document.getElementById("doglist");
+
+fetch(URL)
+  .then(response => response.json())
+  // .then((resultJSON) => console.log(resultJSON)
+  .then((resultJSON) => {
+    for (const dog of resultJSON.dogs) {
+      console.log({ dog });
+    }
+  })
+  .catch((error) => console.error(error.message));
+
+// a resultJSON = a dogs listával
