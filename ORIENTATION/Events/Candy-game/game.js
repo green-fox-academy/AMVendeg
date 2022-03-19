b99f5bcc94dc19fbed9629932389adfa83528b91
jones <mobilizacsio@gmail.com>
2022-03-19 16:52:27 +0100

    create candyCounter and lollypop counter

---
+++ ORIENTATION/Events/Candy-game/game.js
@@ -0,0 +1,31 @@
+// - Gather 10.000 candies!
+// - Clicking the ‘Create candies’ button gives you 1 candy.
+// - You can buy a lollipop for 100 candies by clicking the ‘Buy lollipop’ button.
+// - 10 lollipops generate 1 candy per second.
+//   - Use the 🍭 emoji to display the lollipops you have
+// - Display the candy producton rate in the `Candies / Second` row
+// - If you press the "make candy rain" button, the candy generation should speed up 10x
+
+const createCandyBtn = document.querySelector('.create-candies');
+const candiesNumber = document.querySelector('.candies');
+let candyCounter = 0;
+const buyLollypopsBtn = document.querySelector('.buy-lollypops');
+buyLollypopsBtn.disabled = true;
+const lollypopCounter = document.querySelector('.lollypops');
+let lollyPopIcon = '🍭';
+
+createCandyBtn.addEventListener('click', () => {
+  candyCounter += 1;
+  candiesNumber.textContent = `${candyCounter}`;
+  
+  if (candyCounter >= 10) {
+    buyLollypopsBtn.disabled = false;
+    buyLollypopsBtn.addEventListener('click', () => {
+      lollypopCounter.textContent += lollyPopIcon;
+      candyCounter -= 10;
+      candiesNumber.textContent = `${candyCounter}`;
+    });
+  }
+});
+
+
