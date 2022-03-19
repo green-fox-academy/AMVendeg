653c376a33c9d0a16f65d1aa0c7f3294978bd6f6
jones <mobilizacsio@gmail.com>
2022-03-19 18:24:00 +0100

    candycounter and lollypop counter halfway

--- ORIENTATION/Events/Candy-game/game.js
+++ ORIENTATION/Events/Candy-game/game.js
@@ -2,31 +2,34 @@
 // - Clicking the ‘Create candies’ button gives you 1 candy.
 // - You can buy a lollipop for 100 candies by clicking the ‘Buy lollipop’ button.
 // - 10 lollipops generate 1 candy per second.
-//   - Use the 🍭 emoji to display the lollipops you have
+// - Use the 🍭 emoji to display the lollipops you have
 // - Display the candy producton rate in the `Candies / Second` row
 // - If you press the "make candy rain" button, the candy generation should speed up 10x
 
 const createCandyBtn = document.querySelector('.create-candies');
 const candiesNumber = document.querySelector('.candies');
 let candyCounter = 0;
+
 const buyLollypopsBtn = document.querySelector('.buy-lollypops');
 buyLollypopsBtn.disabled = true;
 const lollypopCounter = document.querySelector('.lollypops');
 let lollyPopIcon = '🍭';
-let lollypopPrice = 100;
+let lollypopPrice = 10;
+
+const candyMachineBtn = document.querySelector('.candy-machine');
+const speed = document.querySelector('.speed');
+let speedCounter = 0;
 
 createCandyBtn.addEventListener('click', () => {
-  candyCounter += 1;
-  candiesNumber.textContent = `${candyCounter}`;
-
-  if (candyCounter >= lollypopPrice) {
+  candyCounter++;
+  
+  if (candyCounter > 10) {
     buyLollypopsBtn.disabled = false;
     buyLollypopsBtn.addEventListener('click', () => {
       lollypopCounter.textContent += lollyPopIcon;
-      candyCounter -= lollypopPrice;
-      candiesNumber.textContent = `${candyCounter}`;
+      candyCounter -= 10;
     });
-  }
+  } 
+  candiesNumber.textContent = candyCounter;
+  
 });
-
-
