3fcfe310c9351edcf12884659b9692b2c68e861a
jones <mobilizacsio@gmail.com>
2022-03-26 14:00:53 +0100

    testing /names endpoint in postman

--- ORIENTATION/Reddit-CA/money-planner/server.ts
+++ ORIENTATION/Reddit-CA/money-planner/server.ts
@@ -1,9 +1,9 @@
 // 0.step npm init -y
 
-import * as express from 'express';
 import * as mysql from 'mysql';
 import { Request, Response } from 'express';
 
+const express = require('express');
 const app = express();
 const PORT = 3000;
 const mySqlErrorMessage = 'Something went wrong, please try again later.';
@@ -38,15 +38,32 @@ const createPlanTableQuery = `
     name VARCHAR(45) NOT NULL);
 `;
 
-
 // create table query error handling
 conn.query(createPlanTableQuery, (err) => {
   if (err) {
-    console.log(err);
+    console.log(mySqlErrorMessage);
     return;
   }
   console.log('Table plan created');
 });
+
+
+// ENDPOINTS
+
+// GET table datas
+app.get('/plan/names', (_req: Request, res: Response) => {
+  conn.query('SELECT id, name FROM plans', (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send(mySqlErrorMessage);
+      return;
+    }
+    res.status(200).send(result);
+  });
+});
+
+
+
 
 
 // app listen
